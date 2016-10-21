76e731badaf37b94de2aec38ea0ceeb64358bc9b
NikolausKrobath <n.krobath@gmail.com>
2016-10-21 08:34:35 +0200

    adminbean und adminindex

--- OnlineGraduateManagement/src/java/bean/AdminBean.java
+++ OnlineGraduateManagement/src/java/bean/AdminBean.java
@@ -38,4 +38,29 @@ public class AdminBean {
         
     }
 
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public List<Klassen> getkList() {
+        return kList;
+    }
+
+    public void setkList(List<Klassen> kList) {
+        this.kList = kList;
+    }
+
+    public List<Jahrgang> getjList() {
+        return jList;
+    }
+
+    public void setjList(List<Jahrgang> jList) {
+        this.jList = jList;
+    }
+    
+
 }
